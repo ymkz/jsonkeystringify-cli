fa170e242d32e879bf279c0cb381d06944953f25
ymkz <ymkz@users.noreply.github.com>
2021-04-16 17:46:26 +0900

    chore: use readTextFile instead of readFile and decoder

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,20 @@ import { exists } from "https://deno.land/std@0.88.0/fs/exists.ts";
 import { jsonkeystringify } from "./helpers.ts";
 
 const parsedArgs = parse(Deno.args);
-const filePath = String(parsedArgs._[0]);
+const file = String(parsedArgs._[0]);
 const key = parsedArgs.key;
 
-if (!(await exists(filePath))) {
-  console.error("[ERROR] No filepath found.");
+if (!file || !(await exists(file))) {
+  console.error("Error: No file found.");
   Deno.exit(1);
 }
 
-const fileData = await Deno.readFile(filePath);
-const decoder = new TextDecoder("utf-8");
-const content = decoder.decode(fileData);
-
+const content = await Deno.readTextFile(file);
 const json = JSON.parse(content);
 const targetObj = json[key];
 
 if (!targetObj) {
-  console.error("[ERROR] No key found.");
+  console.error("Error: No key found.");
   Deno.exit(1);
 }
 
