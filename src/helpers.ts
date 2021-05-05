6bd94268c769540d6f0726f474db0c377c73c0aa
ymkz <ymkz@users.noreply.github.com>
2021-05-05 22:37:09 +0900

    chore: remove any types for lint

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,6 @@
-export function jsonkeystringify(value: any) {
+type Value = Record<string, string>;
+
+export function jsonkeystringify(value: Value) {
   return Object.keys(value)
     .reduce((prev, next) => `${prev} ${next}`)
     .trim();
